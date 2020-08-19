25100197630eb298c74f392e9ae8521378601536
Kurt Dariel <kurt@wizy.io>
2020-08-19 18:35:17 +0800

    Updates

---
+++ precache-manifest.15563b6acf4de4d882b45ac8dde465a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aecaf42ae6e019301690831517ccd19",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b475aecddc6f3f50077a",
+    "url": "/monster-rolodex/static/css/main.fb814ef6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b475aecddc6f3f50077a",
+    "url": "/monster-rolodex/static/js/main.c636444d.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
